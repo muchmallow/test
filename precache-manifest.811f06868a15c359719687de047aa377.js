74edc5716f01b7e8ebf890335d08cfb71cd7ad11
muchmallow <31728584+muchmallow@users.noreply.github.com>
2019-10-10 12:24:41 +0300

    Updates

---
+++ precache-manifest.811f06868a15c359719687de047aa377.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b9f7686982534a82c6b1a7269e7e00e",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "222ddbae6e10711fefac",
+    "url": "/test/static/css/main.f37d43d4.chunk.css"
+  },
+  {
+    "revision": "43a4830b9010f19c02b5",
+    "url": "/test/static/js/2.24ecffe9.chunk.js"
+  },
+  {
+    "revision": "222ddbae6e10711fefac",
+    "url": "/test/static/js/main.41d987b1.chunk.js"
+  },
+  {
+    "revision": "024aa90e4116b5cff313",
+    "url": "/test/static/js/runtime-main.4d5e4058.js"
+  }
+]);
